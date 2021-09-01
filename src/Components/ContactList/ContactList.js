e9370570d6160942494efedeffe8e4eb8a5bddba
Nico Rithner <nico@altvia.com>
2021-09-01 13:08:39 -0600

    Create test 'Renders list of contacts' and render simple list
    Co-authored-by: Nico nicorithner@gmail.com

--- src/Components/ContactList/ContactList.js
+++ src/Components/ContactList/ContactList.js
@@ -1,7 +1,19 @@
+import styles from './styles.module.css';
+
 export const ContactList = ({
   contacts,
   onEditClick,
   onDeleteClick,
 }) => {
-  return <>Contact List</>;
+  return (
+    <div className={styles.main}>
+      {contacts.map((c, index) => (
+        <div key={`row-${index}`} data-testid={`contact-${c.id}`}>
+          <div>{c.name}</div>
+          <div>{c.phone}</div>
+          <div>{c.email}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
--- src/Components/ContactList/ContactList.test.js
+++ src/Components/ContactList/ContactList.test.js
@@ -1,4 +1,37 @@
-import { render, screen, cleanup, fireEvent } from '@testing-library/react';
-import { ContactModal } from './ContactModal';
+import {
+  render,
+  screen,
+  cleanup,
+  fireEvent,
+} from '@testing-library/react';
+import { ContactList } from './ContactList';
 
-describe('', () => {});
+test.only('Renders list of contacts', () => {
+  const contacts = [
+    {
+      id: 0,
+      name: 'Joe',
+      phone: '987-654-3210',
+      email: 'test123@gmail.com',
+    },
+    {
+      id: 1,
+      name: 'Bob',
+      phone: '187-654-3210',
+      email: 'test456@gmail.com',
+    },
+  ];
+
+  render(<ContactList contacts={contacts} />);
+
+  const joeRow = screen.getByTestId('contact-0');
+  const bobRow = screen.getByTestId('contact-1');
+
+  expect(joeRow).toHaveTextContent('Joe');
+  expect(joeRow).toHaveTextContent('987-654-3210');
+  expect(joeRow).toHaveTextContent('test123@gmail.com');
+
+  expect(bobRow).toHaveTextContent('Bob');
+  expect(bobRow).toHaveTextContent('187-654-3210');
+  expect(bobRow).toHaveTextContent('test456@gmail.com');
+});
