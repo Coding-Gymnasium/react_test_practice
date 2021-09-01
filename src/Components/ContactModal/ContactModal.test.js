5b6bae153f14bc61bafe99373bd7f8592531c4f8
Nico Rithner <nico@altvia.com>
2021-09-01 10:50:58 -0600

    Add describe section for create
    Co-authored-by: Nico nicorithner@gmail.com

--- src/Components/ContactModal/ContactModal.test.js
+++ src/Components/ContactModal/ContactModal.test.js
@@ -1,121 +1,121 @@
 import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import { ContactModal } from './ContactModal';
 
-afterEach(cleanup);
+describe('Create Contact', () => {
+  test('Initializes empty form', () => {
+    render(<ContactModal />);
 
-test('Initializes empty form', () => {
-  render(<ContactModal />);
+    const nameInput = screen.getByPlaceholderText('Name');
+    const phoneInput = screen.getByPlaceholderText('Phone Number');
+    const emailInput = screen.getByPlaceholderText('Email Address');
+    const submitButton = screen.getByText('Submit');
 
-  const nameInput = screen.getByPlaceholderText('Name');
-  const phoneInput = screen.getByPlaceholderText('Phone Number');
-  const emailInput = screen.getByPlaceholderText('Email Address');
-  const submitButton = screen.getByText('Submit');
+    expect(nameInput).toBeInTheDocument();
+    expect(phoneInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
 
-  expect(nameInput).toBeInTheDocument();
-  expect(phoneInput).toBeInTheDocument();
-  expect(emailInput).toBeInTheDocument();
+    expect(nameInput).toHaveValue('');
+    expect(phoneInput).toHaveValue('');
+    expect(emailInput).toHaveValue('');
 
-  expect(nameInput).toHaveValue('');
-  expect(phoneInput).toHaveValue('');
-  expect(emailInput).toHaveValue('');
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument();
 
-  expect(screen.queryByTestId('error')).not.toBeInTheDocument();
+    expect(submitButton).toBeDisabled();
+  });
 
-  expect(submitButton).toBeDisabled();
-});
+  test('Enables submit button once form is valid', () => {
+    render(<ContactModal />);
 
-test('Enables submit button once form is valid', () => {
-  render(<ContactModal />);
+    const nameInput = screen.getByPlaceholderText('Name');
+    const phoneInput = screen.getByPlaceholderText('Phone Number');
+    const emailInput = screen.getByPlaceholderText('Email Address');
+    const submitButton = screen.getByText('Submit');
 
-  const nameInput = screen.getByPlaceholderText('Name');
-  const phoneInput = screen.getByPlaceholderText('Phone Number');
-  const emailInput = screen.getByPlaceholderText('Email Address');
-  const submitButton = screen.getByText('Submit');
+    fireEvent.change(nameInput, { target: { value: 'Port Exe' } });
 
-  fireEvent.change(nameInput, { target: { value: 'Port Exe' } });
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument();
 
-  expect(screen.queryByTestId('error')).not.toBeInTheDocument();
+    fireEvent.change(phoneInput, { target: { value: '123-456-7890' } });
 
-  fireEvent.change(phoneInput, { target: { value: '123-456-7890' } });
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument();
 
-  expect(screen.queryByTestId('error')).not.toBeInTheDocument();
+    fireEvent.change(emailInput, { target: { value: '123@gmail.com' } });
 
-  fireEvent.change(emailInput, { target: { value: '123@gmail.com' } });
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument();
 
-  expect(screen.queryByTestId('error')).not.toBeInTheDocument();
+    expect(submitButton).not.toBeDisabled();
+  });
 
-  expect(submitButton).not.toBeDisabled();
-});
+  test('Remains disable if input is not valid', () => {
+    render(<ContactModal />);
 
-test('Remains disable if input is not valid', () => {
-  render(<ContactModal />);
+    const nameInput = screen.getByPlaceholderText('Name');
+    const phoneInput = screen.getByPlaceholderText('Phone Number');
+    const emailInput = screen.getByPlaceholderText('Email Address');
+    const submitButton = screen.getByText('Submit');
 
-  const nameInput = screen.getByPlaceholderText('Name');
-  const phoneInput = screen.getByPlaceholderText('Phone Number');
-  const emailInput = screen.getByPlaceholderText('Email Address');
-  const submitButton = screen.getByText('Submit');
+    fireEvent.change(nameInput, { target: { value: 'Port Exe' } });
+    fireEvent.change(phoneInput, { target: { value: '123-456-7890' } });
+    fireEvent.change(emailInput, { target: { value: '123' } });
 
-  fireEvent.change(nameInput, { target: { value: 'Port Exe' } });
-  fireEvent.change(phoneInput, { target: { value: '123-456-7890' } });
-  fireEvent.change(emailInput, { target: { value: '123' } });
+    expect(submitButton).toBeDisabled();
 
-  expect(submitButton).toBeDisabled();
+    fireEvent.change(phoneInput, { target: { value: '1234567890' } });
 
-  fireEvent.change(phoneInput, { target: { value: '1234567890' } });
+    expect(submitButton).toBeDisabled();
+  });
 
-  expect(submitButton).toBeDisabled();
-});
+  test('Displays error messages for invalid inputs', () => {
+    render(<ContactModal />);
 
-test('Displays error messages for invalid inputs', () => {
-  render(<ContactModal />);
+    const nameInput = screen.getByPlaceholderText('Name');
+    const phoneInput = screen.getByPlaceholderText('Phone Number');
+    const emailInput = screen.getByPlaceholderText('Email Address');
 
-  const nameInput = screen.getByPlaceholderText('Name');
-  const phoneInput = screen.getByPlaceholderText('Phone Number');
-  const emailInput = screen.getByPlaceholderText('Email Address');
+    // email error
+    fireEvent.change(nameInput, { target: { value: 'Port Exe' } });
+    fireEvent.change(phoneInput, { target: { value: '123-456-7890' } });
+    fireEvent.change(emailInput, { target: { value: '123' } });
 
-  // email error
-  fireEvent.change(nameInput, { target: { value: 'Port Exe' } });
-  fireEvent.change(phoneInput, { target: { value: '123-456-7890' } });
-  fireEvent.change(emailInput, { target: { value: '123' } });
+    let errorDiv = screen.queryByTestId('error');
 
-  let errorDiv = screen.queryByTestId('error');
+    expect(errorDiv).toHaveTextContent('Email is improperly formatted');
 
-  expect(errorDiv).toHaveTextContent('Email is improperly formatted');
+    // phone error
+    fireEvent.change(phoneInput, { target: { value: '1234567890' } });
+    fireEvent.change(emailInput, { target: { value: '123@gmail.com' } });
 
-  // phone error
-  fireEvent.change(phoneInput, { target: { value: '1234567890' } });
-  fireEvent.change(emailInput, { target: { value: '123@gmail.com' } });
+    errorDiv = screen.queryByTestId('error');
+    expect(errorDiv).toHaveTextContent('Phone is improperly formatted');
 
-  errorDiv = screen.queryByTestId('error');
-  expect(errorDiv).toHaveTextContent('Phone is improperly formatted');
+    fireEvent.change(phoneInput, { target: { value: '123-456-7890' } });
+    errorDiv = screen.queryByTestId('error');
+    expect(errorDiv).not.toBeInTheDocument();
+  });
 
-  fireEvent.change(phoneInput, { target: { value: '123-456-7890' } });
-  errorDiv = screen.queryByTestId('error');
-  expect(errorDiv).not.toBeInTheDocument();
-});
+  test('Prevents submit function from being called if invalid', () => {
+    const onSubmit = jest.fn();
 
-test('Prevents submit function from being called if invalid', () => {
-  const onSubmit = jest.fn();
+    render(<ContactModal submit={onSubmit} />);
 
-  render(<ContactModal submit={onSubmit} />);
+    const nameInput = screen.getByPlaceholderText('Name');
+    const phoneInput = screen.getByPlaceholderText('Phone Number');
+    const emailInput = screen.getByPlaceholderText('Email Address');
+    const submitButton = screen.getByText('Submit');
+    const form = screen.getByTestId('contact-modal-form');
 
-  const nameInput = screen.getByPlaceholderText('Name');
-  const phoneInput = screen.getByPlaceholderText('Phone Number');
-  const emailInput = screen.getByPlaceholderText('Email Address');
-  const submitButton = screen.getByText('Submit');
-  const form = screen.getByTestId('contact-modal-form');
+    fireEvent.change(nameInput, { target: { value: 'Port Exe' } });
+    fireEvent.change(phoneInput, { target: { value: '123-456-7890' } });
+    fireEvent.change(emailInput, { target: { value: '123@gmail.' } });
 
-  fireEvent.change(nameInput, { target: { value: 'Port Exe' } });
-  fireEvent.change(phoneInput, { target: { value: '123-456-7890' } });
-  fireEvent.change(emailInput, { target: { value: '123@gmail.' } });
+    expect(submitButton).toBeDisabled();
+    fireEvent.submit(form);
+    expect(onSubmit).not.toHaveBeenCalled();
 
-  expect(submitButton).toBeDisabled();
-  fireEvent.submit(form);
-  expect(onSubmit).not.toHaveBeenCalled();
+    fireEvent.change(emailInput, { target: { value: '123@gmail.com' } });
 
-  fireEvent.change(emailInput, { target: { value: '123@gmail.com' } });
-
-  expect(submitButton).not.toBeDisabled();
-  fireEvent.submit(form);
-  expect(onSubmit).toHaveBeenCalled();
+    expect(submitButton).not.toBeDisabled();
+    fireEvent.submit(form);
+    expect(onSubmit).toHaveBeenCalled();
+  });
 });
