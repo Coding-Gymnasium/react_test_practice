01524634af190173a35f7755436940ecce4ff564
Nico Rithner <nicorithner@gmail.com>
2021-09-05 19:58:57 -0600

    Create test for create user

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactModal } from '../ContactModal/ContactModal';
 
 import styles from './styles.module.css';
 
 export const App = () => {
+  const [contacts, setContacts] = useState();
   const [addingContact, setAddingContact] = useState(false);
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (!storedContacts) {
+      localStorage.setItem('contacts', JSON.stringify([]));
+      setContacts([]);
+    } else {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
 
   return (
     <div className={styles.main}>
@@ -12,7 +23,12 @@ export const App = () => {
         <ContactModal
           cancel={() => setAddingContact(false)}
           submit={c => {
-            console.log(c);
+            const newContacts = [...contacts, c];
+            localStorage.setItem(
+              'contacts',
+              JSON.stringify(newContacts),
+            );
+            setContacts(newContacts);
             setAddingContact(false);
           }}
         />
--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -72,3 +72,100 @@ test('Closes modal automatically after submit', () => {
     screen.queryByTestId('contact-modal-form'),
   ).not.toBeInTheDocument();
 });
+
+describe('Local Storage Logic', () => {
+  beforeEach(() => {
+    Object.defineProperty(window, 'localStorage', {
+      value: {
+        getItem: jest.fn(() => null),
+        setItem: jest.fn(c => c),
+      },
+      writable: true,
+    });
+  });
+
+  test('Initializes empty array in localStorage if no contacts are stored yet', () => {
+    render(<App />);
+    expect(window.localStorage.setItem).toHaveBeenCalledWith(
+      'contacts',
+      JSON.stringify([]),
+    );
+  });
+
+  test('Properly stores submitted users', () => {
+    const joe = {
+      name: 'Joe',
+      email: 'joe@gmail.com',
+      phone: '303-333-3333',
+    };
+    const bob = {
+      name: 'Bob',
+      email: 'bob@gmail.com',
+      phone: '303-333-3333',
+    };
+
+    render(<App />);
+
+    expect(
+      screen.queryByTestId('contact-modal-form'),
+    ).not.toBeInTheDocument();
+
+    const addContactBtn = screen.getByTestId('add-contact-btn');
+
+    fireEvent.click(addContactBtn);
+
+    expect(
+      screen.getByTestId('contact-modal-form'),
+    ).toBeInTheDocument();
+
+    let nameInput = screen.getByPlaceholderText('Name');
+    let phoneInput = screen.getByPlaceholderText('Phone Number');
+    let emailInput = screen.getByPlaceholderText('Email Address');
+    let form = screen.getByTestId('contact-modal-form');
+
+    fireEvent.change(phoneInput, { target: { value: joe.phone } });
+    fireEvent.change(nameInput, { target: { value: joe.name } });
+    fireEvent.change(emailInput, {
+      target: { value: joe.email },
+    });
+
+    fireEvent.submit(form);
+
+    expect(
+      screen.queryByTestId('contact-modal-form'),
+    ).not.toBeInTheDocument();
+
+    expect(window.localStorage.setItem).toHaveBeenCalledWith(
+      'contacts',
+      JSON.stringify([joe]),
+    );
+
+    fireEvent.click(addContactBtn);
+
+    expect(
+      screen.getByTestId('contact-modal-form'),
+    ).toBeInTheDocument();
+
+    nameInput = screen.getByPlaceholderText('Name');
+    phoneInput = screen.getByPlaceholderText('Phone Number');
+    emailInput = screen.getByPlaceholderText('Email Address');
+    form = screen.getByTestId('contact-modal-form');
+
+    fireEvent.change(phoneInput, { target: { value: bob.phone } });
+    fireEvent.change(nameInput, { target: { value: bob.name } });
+    fireEvent.change(emailInput, {
+      target: { value: bob.email },
+    });
+
+    fireEvent.submit(form);
+
+    expect(
+      screen.queryByTestId('contact-modal-form'),
+    ).not.toBeInTheDocument();
+
+    expect(window.localStorage.setItem).toHaveBeenCalledWith(
+      'contacts',
+      JSON.stringify([joe, bob]),
+    );
+  });
+});
